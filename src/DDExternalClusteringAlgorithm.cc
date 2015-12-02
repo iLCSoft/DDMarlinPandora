2eadf6859e12394d7f99e8003d91cafea03e83c0
Nikiforos Nikiforou <Nikiforos.Nikiforou@cern.ch>
2015-12-02 09:17:52 +0000

    Adding missing files

---
+++ src/DDExternalClusteringAlgorithm.cc
@@ -0,0 +1,121 @@
+/**
+ *  @file   DDMarlinPandora/src/DDExternalClusteringAlgorithm.cc
+ * 
+ *  @brief  Implementation of the external clustering algorithm class.
+ * 
+ *  $Log: $
+ */
+
+#include "EVENT/LCCollection.h"
+#include "EVENT/LCEvent.h"
+#include "EVENT/Cluster.h"
+
+#include "DDExternalClusteringAlgorithm.h"
+#include "DDPandoraPFANewProcessor.h"
+
+#include "Pandora/AlgorithmHeaders.h"
+
+using namespace pandora;
+
+DDExternalClusteringAlgorithm::DDExternalClusteringAlgorithm() :
+    m_flagClustersAsPhotons(true)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DDExternalClusteringAlgorithm::Run()
+{
+    try
+    {
+        const CaloHitList *pCaloHitList = NULL;
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pCaloHitList));
+
+        if (pCaloHitList->empty())
+            return STATUS_CODE_SUCCESS;
+
+        // Get external photon cluster collection
+        const EVENT::LCEvent *const pLCEvent(DDPandoraPFANewProcessor::GetCurrentEvent(&(this->GetPandora())));
+        const EVENT::LCCollection *const pExternalClusterCollection = pLCEvent->getCollection(m_externalClusterCollectionName);
+        const unsigned int nExternalClusters(pExternalClusterCollection->getNumberOfElements());
+
+        if (0 == nExternalClusters)
+            return STATUS_CODE_SUCCESS;
+
+        // Populate pandora parent address to calo hit map
+        ParentAddressToCaloHitMap parentAddressToCaloHitMap;
+
+        for (CaloHitList::const_iterator hitIter = pCaloHitList->begin(), hitIterEnd = pCaloHitList->end(); hitIter != hitIterEnd; ++hitIter)
+        {
+            const pandora::CaloHit *const pCaloHit = *hitIter;
+            parentAddressToCaloHitMap.insert(ParentAddressToCaloHitMap::value_type(pCaloHit->GetParentCaloHitAddress(), pCaloHit));
+        }
+
+        // Recreate external clusters within the pandora framework
+        for (unsigned int iCluster = 0; iCluster < nExternalClusters; ++iCluster)
+        {
+            const EVENT::Cluster *const pExternalCluster = dynamic_cast<const EVENT::Cluster*>(pExternalClusterCollection->getElementAt(iCluster));
+
+            if (NULL == pExternalCluster)
+                throw EVENT::Exception("Collection type mismatch");
+
+            const CalorimeterHitVec &calorimeterHitVec(pExternalCluster->getCalorimeterHits());
+
+            const pandora::Cluster *pPandoraCluster = NULL;
+
+            for (CalorimeterHitVec::const_iterator iter = calorimeterHitVec.begin(), iterEnd = calorimeterHitVec.end(); iter != iterEnd; ++iter)
+            {
+                ParentAddressToCaloHitMap::const_iterator pandoraCaloHitIter = parentAddressToCaloHitMap.find(*iter);
+
+                if (parentAddressToCaloHitMap.end() == pandoraCaloHitIter)
+                {
+                    continue;
+                }
+
+                const pandora::CaloHit *const pPandoraCaloHit = pandoraCaloHitIter->second;
+
+                if (NULL == pPandoraCluster)
+                {
+                    PandoraContentApi::Cluster::Parameters parameters;
+                    parameters.m_caloHitList.insert(pPandoraCaloHit);
+                    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::Cluster::Create(*this, parameters, pPandoraCluster));
+
+                    if (m_flagClustersAsPhotons)
+                    {
+                        PandoraContentApi::Cluster::Metadata metadata;
+                        metadata.m_particleId = PHOTON;
+                        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::AlterMetadata(*this, pPandoraCluster, metadata));
+                    }
+                }
+                else
+                {
+                    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::AddToCluster(*this, pPandoraCluster, pPandoraCaloHit));
+                }
+            }
+        }
+    }
+    catch (StatusCodeException &statusCodeException)
+    {
+        return statusCodeException.GetStatusCode();
+    }
+    catch (EVENT::Exception &exception)
+    {
+        std::cout << "DDExternalClusteringAlgorithm failure: " << exception.what() << std::endl;
+        return STATUS_CODE_FAILURE;
+    }
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DDExternalClusteringAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle,
+        "ExternalClusterCollectionName", m_externalClusterCollectionName));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+        "FlagClustersAsPhotons", m_flagClustersAsPhotons));
+
+    return STATUS_CODE_SUCCESS;
+}
---
+++ src/DDPfoCreator.cc
@@ -0,0 +1,161 @@
+/**
+ *  @file   DDMarlinPandora/src/DDPfoCreator.cc
+ * 
+ *  @brief  Implementation of the pfo creator class.
+ * 
+ *  $Log: $
+ */
+
+#include "marlin/Global.h"
+#include "marlin/Processor.h"
+
+#include "EVENT/LCCollection.h"
+
+#include "IMPL/ClusterImpl.h"
+#include "IMPL/LCCollectionVec.h"
+#include "IMPL/LCFlagImpl.h"
+#include "IMPL/LCGenericObjectImpl.h"
+#include "IMPL/LCRelationImpl.h"
+#include "IMPL/ReconstructedParticleImpl.h"
+
+#include "CalorimeterHitType.h"
+#include "ClusterShapes.h"
+
+#include "Api/PandoraApi.h"
+
+#include "Objects/ParticleFlowObject.h"
+
+#include "DDPandoraPFANewProcessor.h"
+#include "DDPfoCreator.h"
+
+#include <cmath>
+
+DDPfoCreator::DDPfoCreator(const Settings &settings, const pandora::Pandora *const pPandora) :
+    m_settings(settings),
+    m_pPandora(pPandora)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+DDPfoCreator::~DDPfoCreator()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+pandora::StatusCode DDPfoCreator::CreateParticleFlowObjects(EVENT::LCEvent *pLCEvent)
+{
+    const pandora::PfoList *pPfoList = NULL;
+    PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, PandoraApi::GetCurrentPfoList(*m_pPandora, pPfoList));
+
+    IMPL::LCCollectionVec *pClusterCollection = new IMPL::LCCollectionVec(LCIO::CLUSTER);
+    IMPL::LCCollectionVec *pReconstructedParticleCollection = new IMPL::LCCollectionVec(LCIO::RECONSTRUCTEDPARTICLE);
+
+    IMPL::LCFlagImpl lcFlagImpl(pClusterCollection->getFlag());
+    lcFlagImpl.setBit(LCIO::CLBIT_HITS);
+    pClusterCollection->setFlag(lcFlagImpl.getFlag());
+
+    std::vector<std::string> subDetectorNames ;
+    subDetectorNames.push_back("ecal") ; const unsigned int ecal_Index(0) ;
+    subDetectorNames.push_back("hcal") ; const unsigned int hcal_Index(1) ;
+    subDetectorNames.push_back("yoke") ; const unsigned int yoke_Index(2) ;
+    subDetectorNames.push_back("lcal") ; const unsigned int lcal_Index(3) ;
+    subDetectorNames.push_back("lhcal"); const unsigned int lhcal_Index(4);
+    subDetectorNames.push_back("bcal") ; const unsigned int bcal_Index(5) ;
+
+    pClusterCollection->parameters().setValues("ClusterSubdetectorNames", subDetectorNames);
+
+    // Create lcio "reconstructed particles" from the pandora "particle flow objects"
+    for (pandora::PfoList::const_iterator itPFO = pPfoList->begin(), itPFOEnd = pPfoList->end(); itPFO != itPFOEnd; ++itPFO)
+    {
+        IMPL::ReconstructedParticleImpl *pReconstructedParticle = new ReconstructedParticleImpl();
+
+        const pandora::ClusterAddressList clusterAddressList((*itPFO)->GetClusterAddressList());
+        const pandora::TrackAddressList trackAddressList((*itPFO)->GetTrackAddressList());
+
+        // Create lcio clusters
+        for (pandora::ClusterAddressList::const_iterator itCluster = clusterAddressList.begin(), itClusterEnd = clusterAddressList.end();
+            itCluster != itClusterEnd; ++itCluster)
+        {
+            IMPL::ClusterImpl *pCluster = new ClusterImpl();
+
+            const unsigned int nHitsInCluster((*itCluster).size());
+
+            float clusterEnergy(0.);
+            float *pHitE = new float[nHitsInCluster];
+            float *pHitX = new float[nHitsInCluster];
+            float *pHitY = new float[nHitsInCluster];
+            float *pHitZ = new float[nHitsInCluster];
+
+            for (unsigned int iHit = 0; iHit < nHitsInCluster; ++iHit)
+            {
+                EVENT::CalorimeterHit *pCalorimeterHit = (CalorimeterHit*)((*itCluster)[iHit]);
+                pCluster->addHit(pCalorimeterHit, 1.0);
+
+                const float caloHitEnergy(pCalorimeterHit->getEnergy());
+                clusterEnergy += caloHitEnergy;
+
+                pHitE[iHit] = caloHitEnergy;
+                pHitX[iHit] = pCalorimeterHit->getPosition()[0];
+                pHitY[iHit] = pCalorimeterHit->getPosition()[1];
+                pHitZ[iHit] = pCalorimeterHit->getPosition()[2];
+
+                std::vector<float> &subDetectorEnergies = pCluster->subdetectorEnergies();
+                subDetectorEnergies.resize(subDetectorNames.size());
+
+                switch (CHT(pCalorimeterHit->getType()).caloID())
+                {
+                    case CHT::ecal:  subDetectorEnergies[ecal_Index ] += caloHitEnergy; break;
+                    case CHT::hcal:  subDetectorEnergies[hcal_Index ] += caloHitEnergy; break;
+                    case CHT::yoke:  subDetectorEnergies[yoke_Index ] += caloHitEnergy; break;
+                    case CHT::lcal:  subDetectorEnergies[lcal_Index ] += caloHitEnergy; break;
+                    case CHT::lhcal: subDetectorEnergies[lhcal_Index] += caloHitEnergy; break;
+                    case CHT::bcal:  subDetectorEnergies[bcal_Index ] += caloHitEnergy; break;
+                    default: streamlog_out(DEBUG) << " no subdetector found for hit with type: " << pCalorimeterHit->getType() << std::endl;
+                }
+            }
+
+            pCluster->setEnergy(clusterEnergy);
+
+            ClusterShapes *pClusterShapes = new ClusterShapes(nHitsInCluster, pHitE, pHitX, pHitY, pHitZ);
+            pCluster->setPosition(pClusterShapes->getCentreOfGravity());
+            pCluster->setIPhi(std::atan2(pClusterShapes->getEigenVecInertia()[1], pClusterShapes->getEigenVecInertia()[0]));
+            pCluster->setITheta(std::acos(pClusterShapes->getEigenVecInertia()[2]));
+
+            pClusterCollection->addElement(pCluster);
+            pReconstructedParticle->addCluster(pCluster);
+
+            delete pClusterShapes;
+            delete[] pHitE; delete[] pHitX; delete[] pHitY; delete[] pHitZ;
+        }
+
+        // Add tracks to the lcio reconstructed particles
+        for (pandora::TrackAddressList::const_iterator itTrack = trackAddressList.begin(), itTrackEnd = trackAddressList.end();
+            itTrack != itTrackEnd; ++itTrack)
+        {
+            pReconstructedParticle->addTrack((Track*)(*itTrack));
+        }
+
+        float momentum[3] = {(*itPFO)->GetMomentum().GetX(), (*itPFO)->GetMomentum().GetY(), (*itPFO)->GetMomentum().GetZ()};
+        pReconstructedParticle->setMomentum(momentum);
+        pReconstructedParticle->setEnergy((*itPFO)->GetEnergy());
+        pReconstructedParticle->setMass((*itPFO)->GetMass());
+        pReconstructedParticle->setCharge((*itPFO)->GetCharge());
+        pReconstructedParticle->setType((*itPFO)->GetParticleId());
+
+        pReconstructedParticleCollection->addElement(pReconstructedParticle);
+    }
+
+    pLCEvent->addCollection(pClusterCollection, m_settings.m_clusterCollectionName.c_str());
+    pLCEvent->addCollection(pReconstructedParticleCollection, m_settings.m_pfoCollectionName.c_str());
+
+    return pandora::STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+DDPfoCreator::Settings::Settings()
+{
+}
