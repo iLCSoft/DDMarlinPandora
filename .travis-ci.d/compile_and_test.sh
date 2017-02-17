ae858ec30b964440ec467a7b88c29740ac4583fc
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-02-17 13:20:40 +0100

    Add travis config

---
+++ .travis-ci.d/compile_and_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
+source $ILCSOFT/init_ilcsoft.sh
+
+cd /Package
+mkdir build
+cd build
+cmake -GNinja -C $ILCSOFT/ILCSoft.cmake ..
+ninja
